8c504a9112ff25323528e1c64e81b6f261ecdf96
Owen Kaluza <owen.kaluza@monash.edu>
2022-11-23 12:18:26 +1100

    Revert to ubuntu 20.04 cuda 11.2 base for gpu image
    Rework build to get the scripts in github-action (hopefully this
    works)

---
+++ test_build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+./update_base.sh
+
+#https://hub.docker.com/r/nvidia/cuda/
+#These require a newer driver
+#NOTE: pytorch requires 11.6 for stable, 11.7 for nightly currently, only 11.7 has 22.04 build
+#NOTE: cudann8 vs bare 11.8.0-runtime-ubuntu22.04 is ~500GB larger
+#CUDA_IMAGE=cuda:11.8.0-cudnn8-runtime-ubuntu22.04
+#CUDA_IMAGE=cuda:11.7.0-cudnn8-runtime-ubuntu22.04
+CUDA_IMAGE=cuda:11.2.2-cudnn8-runtime-ubuntu20.04
+
+sed -i "s/ROOT_CONTAINER=nvidia.*/ROOT_CONTAINER=nvidia\/${CUDA_IMAGE}/g" .github/workflows/build-images.yaml
+
+ROOT=nvidia/${CUDA_IMAGE}
+ROOT=ubuntu:20.04
+
+cd docker-stacks-foundation
+docker build -t docker-stacks-foundation --build-arg ROOT_CONTAINER=${ROOT} -f Dockerfile .
+cd ..
+
+cd base-notebook
+docker build -t base-notebook --build-arg BASE_CONTAINER=docker-stacks-foundation -f Dockerfile .
+cd ..
+
+cd minimal-notebook
+docker build -t minimal-notebook --build-arg BASE_CONTAINER=base-notebook -f Dockerfile .
+cd ..
+
+cd base
+docker build -t pipeline-base --build-arg BASE_CONTAINER=minimal-notebook -f Dockerfile .
+
+#TEST
+#docker run -p 8888:8888 pipeline-base
+
+#cd ../fd
+#docker build -t pipeline-fd --build-arg BASE_CONTAINER=pipeline-base -f Dockerfile .
+
+#TEST
+#docker run -p 8888:8888 pipeline-exp
--- update_base.sh
+++ update_base.sh
@@ -1,45 +1,23 @@
 #!/bin/bash
+#Get the jupyter/docker-stacks files
+#SRC=https://raw.githubusercontent.com/jupyter/docker-stacks/main/
+SRC=https://raw.githubusercontent.com/AuScalableDroneCloud/docker-stacks/main/
+
+mkdir -p docker-stacks-foundation
+
+wget ${SRC}docker-stacks-foundation/Dockerfile -O docker-stacks-foundation/Dockerfile
+wget ${SRC}docker-stacks-foundation/fix-permissions -O docker-stacks-foundation/fix-permissions
+wget ${SRC}docker-stacks-foundation/initial-condarc -O docker-stacks-foundation/initial-condarc
+wget ${SRC}docker-stacks-foundation/start.sh -O docker-stacks-foundation/start.sh
+
 mkdir -p base-notebook
-cd base-notebook
-wget https://raw.githubusercontent.com/jupyter/docker-stacks/main/LICENSE.md -O LICENSE_Dockerfile.md
-wget https://raw.githubusercontent.com/jupyter/docker-stacks/main/base-notebook/Dockerfile -O Dockerfile
-wget https://raw.githubusercontent.com/jupyter/docker-stacks/main/base-notebook/fix-permissions -O fix-permissions
-wget https://raw.githubusercontent.com/jupyter/docker-stacks/main/base-notebook/initial-condarc -O initial-condarc
-wget https://raw.githubusercontent.com/jupyter/docker-stacks/main/base-notebook/jupyter_server_config.py -O jupyter_server_config.py
-wget https://raw.githubusercontent.com/jupyter/docker-stacks/main/base-notebook/start-notebook.sh -O start-notebook.sh
-wget https://raw.githubusercontent.com/jupyter/docker-stacks/main/base-notebook/start-singleuser.sh -O start-singleuser.sh
-wget https://raw.githubusercontent.com/jupyter/docker-stacks/main/base-notebook/start.sh -O start.sh
-
-#https://hub.docker.com/r/nvidia/cuda/
-#These require a newer driver
-#NOTE: pytorch requires 11.6 for stable, 11.7 for nightly currently, only 11.7 has 22.04 build
-#NOTE: cudann8 vs bare 11.8.0-runtime-ubuntu22.04 is ~500GB larger
-#CUDA_IMAGE=cuda:11.8.0-cudnn8-runtime-ubuntu22.04
-#CUDA_IMAGE=cuda:11.7.0-cudnn8-runtime-ubuntu22.04
-CUDA_IMAGE=cuda:11.2.2-cudnn8-runtime-ubuntu20.04
-
-sed -i "s/ROOT_CONTAINER=nvidia.*/ROOT_CONTAINER=nvidia\/${CUDA_IMAGE}/g" .github/workflows/build-images.yaml
-
-docker build -t base-notebook --build-arg ROOT_CONTAINER=nvidia/${CUDA_IMAGE} -f Dockerfile .
-cd ..
+wget ${SRC}base-notebook/Dockerfile -O base-notebook/Dockerfile
+wget ${SRC}base-notebook/jupyter_server_config.py -O base-notebook/jupyter_server_config.py
+wget ${SRC}base-notebook/start-notebook.sh -O base-notebook/start-notebook.sh
+wget ${SRC}base-notebook/start-singleuser.sh -O base-notebook/start-singleuser.sh
 
 mkdir -p minimal-notebook
-cd minimal-notebook
-wget https://raw.githubusercontent.com/jupyter/docker-stacks/main/minimal-notebook/Dockerfile -O Dockerfile
-wget https://raw.githubusercontent.com/jupyter/docker-stacks/main/minimal-notebook/Rprofile.site -O Rprofile.site
+wget ${SRC}minimal-notebook/Dockerfile -O minimal-notebook/Dockerfile
+wget ${SRC}minimal-notebook/Rprofile.site -O minimal-notebook/Rprofile.site
 
-docker build -t minimal-notebook --build-arg BASE_CONTAINER=base-notebook -f Dockerfile .
-cd ..
-
-cd base
-docker build -t pipeline-base --build-arg BASE_CONTAINER=minimal-notebook -f Dockerfile .
-
-#TEST
-#docker run -p 8888:8888 pipeline-base
-
-cd ../experiments
-docker build -t pipeline-exp --build-arg BASE_CONTAINER=pipeline-base -f Dockerfile .
-
-#TEST
-#docker run -p 8888:8888 pipeline-exp
 
